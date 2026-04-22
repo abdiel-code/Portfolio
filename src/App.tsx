73bf0baf247dd6670715344467afc404f36991f0
abdiel-codes <abdieljflores.dev@gmail.com>
2026-04-21 20:23:43 -0600

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+function App() {
+  return (
+    <div>
+      {/*WaterCanvas*/}
+      <nav></nav>
+      <main>
+        {/*Hero*/}
+        {/*Projects*/}
+        {/*Skills*/}
+        {/*Contact*/}
+      </main>
+    </div>
+  );
+}
+
+export default App;
